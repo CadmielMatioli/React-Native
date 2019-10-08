78686e8b6145dc3b94f4db4dce4f5266c1c215d2
Cadmiel Matioli Donato <cadmatioli@gmail.com>
2019-10-08 18:32:40 -0300

    Adicionado titulo e backgound

---
+++ Picker/src/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text} from 'react-native';
+
+import { Title, Container } from './styles';
+
+export default function Main() {
+return( 
+    <View>
+        <Container>
+            <Title> Picker </Title>
+        </Container>
+        </View>
+);
+}
