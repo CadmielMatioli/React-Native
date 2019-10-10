41a5f3b9c4d55d246201c1517bcfb880e525f2ad
Cadmiel Matioli Donato <cadmatioli@gmail.com>
2019-10-10 18:05:31 -0300

    b

--- Picker/src/Main/index.js
+++ Picker/src/Main/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
-import { View, Text} from 'react-native';
+import { View, Text, StatusBar, Image} from 'react-native';
 
 import { Title, Container } from './styles';
 
 export default function Main() {
     return( 
-        <View>
-            <Container>
-                <Title> Picker </Title>
+        <View style={{flex:1, backgroundColor:'#FFF'}}>
+            <StatusBar backgroundColor="#FFF"  barStyle="dark-content" />
+            <Container style={{
+                    alignSelf:"center",
+                }}>
+                <Title> Picker Love</Title>
+                <Image  
+                style={{
+                    width: 50, 
+                    height: 50, 
+                    borderRadius:100,
+                    padding:10
+                }} 
+                source={
+                    require('../images/img_avatar.png')
+                }/>
             </Container>
         </View>
     );
--- Picker/src/Main/styles.js
+++ Picker/src/Main/styles.js
@@ -1,21 +1,23 @@
 import styled from 'styled-components/native';
-import { expression } from '@babel/template';
 
 export const Title = styled.Text`
-    position: absolute;
+    position: relative;
     width: 100%;
-    top: 25%;
     font-style: normal;
     font-weight: normal;
     font-size: 24px;
-    line-height: 24px;
     text-align: center;
-    color: #FFFFFF;
+    top:15%;
+    color: #fFF;
 `;
 
 export const Container = styled.View`
-    position: absolute;
-    width: 100%;
-    height: 150px;
+    position: relative;
+    align-items:center;
+    border-radius:100px;
+    align-self: center;
+    width: 80%;
+    height:50px;
+    margin:5px;
     background: #5A0F96;
 `;
