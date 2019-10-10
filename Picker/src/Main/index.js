f7b496f674ed0f5221267a933bd0a5ab10558653
Cadmiel Matioli Donato <cadmatioli@gmail.com>
2019-10-10 16:06:13 -0300

    a

--- Picker/src/Main/index.js
+++ Picker/src/Main/index.js
@@ -5,11 +5,11 @@ import { View, Text} from 'react-native';
 import { Title, Container } from './styles';
 
 export default function Main() {
-return( 
-    <View>
-        <Container>
-            <Title> Picker </Title>
-        </Container>
+    return( 
+        <View>
+            <Container>
+                <Title> Picker </Title>
+            </Container>
         </View>
-);
+    );
 }
