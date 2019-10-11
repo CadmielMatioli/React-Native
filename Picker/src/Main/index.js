a1cb93adcceba0556441a71352a1c6decc0248ea
Cadmiel Matioli Donato <cadmatioli@gmail.com>
2019-10-11 10:02:21 -0300

    instalado 2 bibliotecas para rotas

--- Picker/src/Main/index.js
+++ Picker/src/Main/index.js
@@ -4,18 +4,32 @@ import { View, Text, StatusBar, ScrollView} from 'react-native';
 
 import { Title, Container , Input, Image} from './styles';
 
+import { createAppContainer } from 'react-navigation';
+
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Page1 from '../Routes/Page1';
+import Page2 from '../Routes/Page2';
+
 export default function Main() {
-    return( 
-        <View style={{flex:1, backgroundColor:'#FFF'}}>
+    const Routes = createAppContainer(
+        createStackNavigator({
+          Home: Page1,
+          About: Page2,
+        })
+      );
+      return( 
+          
+          <View style={{flex:1, backgroundColor:'#FFF'}}>
                 <StatusBar backgroundColor="#FFF"  barStyle="dark-content" />
                 {/* <ScrollView>
                 <Text>
-                    iwdsidfhskjfhdk
+                iwdsidfhskjfhdk
                 </Text>
             </ScrollView> */}
                 <Container style={{
-                        alignSelf:"center",
-                    }}>
+                    alignSelf:"center",
+                }}>
                     <Title> Picker Love</Title>
                     <Image  
                     source={
@@ -25,7 +39,6 @@ export default function Main() {
                     source={
                         require('../images/img_avatar2.png')
                     }/>
-          
                 </Container>
         </View>
     );
